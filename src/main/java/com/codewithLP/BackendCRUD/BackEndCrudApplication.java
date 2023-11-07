6d8db5d03c5734f74ec476ab9ba7696973ebec2a
LuuPhuc01 <phucluu7271@gmail.com>
2023-11-07 15:47:59 +0700

    first commit

---
+++ src/main/java/com/codewithLP/BackendCRUD/BackEndCrudApplication.java
@@ -0,0 +1,13 @@
+package com.codewithLP.BackendCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndCrudApplication.class, args);
+	}
+
+}
